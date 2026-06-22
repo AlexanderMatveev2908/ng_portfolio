a881aff800f3cccdd474c03bbdcebd296f9d8425
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-22 09:37:53 +0300

    main => _

---
+++ apps/Client/src/core/constants/breakpoints.ts
@@ -0,0 +1,11 @@
+export class Breakpoints {
+  public static readonly SM = 640;
+
+  public static readonly MD = 768;
+
+  public static readonly LG = 1024;
+
+  public static readonly XL = 1280;
+
+  public static readonly XXL = 1536;
+}
--- apps/Client/src/pages/home_page/components/projects/projects.ts
+++ apps/Client/src/pages/home_page/components/projects/projects.ts
@@ -1,8 +1,19 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  HostListener,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { FilterAppType } from './components/filter_app_type/filter-app-type';
 import { ProjectsSlice } from '@/features/projects/slice';
 import { ProjectsUiFct } from '@/features/projects/ui_fct';
 import { ImgLoading } from '@/common/components/general/img_loading/img-loading';
+import { UsePlatformSvc } from '@/core/services/use_platform';
+import { Breakpoints } from '@/core/constants/breakpoints';
 
 @Component({
   selector: 'app-projects',
@@ -11,10 +22,43 @@ import { ImgLoading } from '@/common/components/general/img_loading/img-loading'
   styleUrl: './projects.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Projects {
+export class Projects implements OnInit {
+  public readonly limit: WritableSignal<number> = signal(1);
+
+  private readonly usePlatform: UsePlatformSvc = inject(UsePlatformSvc);
   private readonly projectsSlice: ProjectsSlice = inject(ProjectsSlice);
 
   public readonly filtered = computed(() =>
     ProjectsUiFct.data.filter((el) => this.projectsSlice.appType().includes(el.appType)),
   );
+
+  public readonly paginated = computed(() => {
+    const page = this.projectsSlice.page();
+    const limit = this.limit();
+
+    const start = page * limit;
+    const end = start + limit;
+
+    return this.filtered().slice(start, end);
+  });
+
+  public updateLimit(): void {
+    const width = window.innerWidth;
+
+    if (this.usePlatform.isServer) {
+      this.limit.set(1);
+    } else {
+      if (width > Breakpoints.XXL) this.limit.set(4);
+      else if (width > Breakpoints.XL) this.limit.set(3);
+      else if (width > Breakpoints.MD) this.limit.set(2);
+      else this.limit.set(1);
+    }
+  }
+  ngOnInit(): void {
+    this.updateLimit();
+  }
+  @HostListener('window:resize')
+  public onResize() {
+    this.updateLimit();
+  }
 }
