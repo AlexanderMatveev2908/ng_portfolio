3aeeba6a95d72f3b0e73d3671c2d4865ae9e7e83
AlexanderMatveev2908 <matveevalexander470@gmail.com>
2026-06-22 11:18:43 +0300

    main => _

--- apps/Client/src/pages/home_page/home-page.ts
+++ apps/Client/src/pages/home_page/home-page.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Hero } from './components/hero/hero';
 import { TechStack } from './components/tech_stack/tech-stack';
 import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
 
 @Component({
   selector: 'app-home-page',
-  imports: [Hero, TechStack, Projects],
+  imports: [Hero, TechStack, Projects, Contact],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
